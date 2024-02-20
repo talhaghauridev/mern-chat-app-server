f2f4927a3d329024762bba055d5cc2923755a7cb
Talha Ghouri <talhaghouridev@gmail.com>
2024-02-20 12:13:02 -0800

    add config file

--- src/index.js
+++ src/index.js
@@ -4,13 +4,10 @@ const cloudinary = require("cloudinary");
 const { Server } = require("socket.io");
 const connectDB = require("./db/index.js");
 const socket = require("./socket.js");
+const { cloudinayConfig, corsConfig } = require("./config/index.js");
 
 connectDB();
-cloudinary.config({
-  cloud_name: process.env.CLOUDINARY_NAME,
-  api_key: process.env.CLOUDINARY_API_KEY,
-  api_secret: process.env.CLOUDINARY_API_KEY_SECRET,
-});
+cloudinary.config(cloudinayConfig);
 const PORT = process.env.PORT | 5500;
 
 app.get("/", (req, res) => {
@@ -20,19 +17,10 @@ app.get("/", (req, res) => {
   });
 });
 const server = createServer(app);
-const io = new Server(server, {
-  cors: {
-    origin: process.env.FRONTEND_URL,
-    methods: ["GET", "POST"],
-    credentials: true,
-  },
-});
+const io = new Server(server, corsConfig);
 
 socket(io);
 
-if (!module.parent) {
-  server.listen(PORT, () => {
-    console.log(`Server is running on Port: http://localhost:${PORT}`);
-  })};
-
-module.exports = app;
+server.listen(PORT, () => {
+  console.log(`Server is running on Port: http://localhost:${PORT}`);
+});
