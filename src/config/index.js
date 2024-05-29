9a297164057fd5776f57d3644d49aa911e7a678f
Talha Ghauri <talhaghauridev@gmail.com>
2024-05-29 06:43:54 -0700

    add minor change

---
+++ src/config/index.js
@@ -0,0 +1,18 @@
+const cloudinayConfig = {
+  cloud_name: process.env.CLOUDINARY_NAME,
+  api_key: process.env.CLOUDINARY_API_KEY,
+  api_secret: process.env.CLOUDINARY_API_KEY_SECRET,
+};
+
+const corsConfig = {
+  cors: {
+    origin: process.env.FRONTEND_URL,
+    methods: ["GET", "POST", "PUT", "DELETE"],
+    credentials: true,
+  },
+};
+
+module.exports = {
+  cloudinayConfig,
+  corsConfig,
+};
